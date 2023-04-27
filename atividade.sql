472cde8452b4ca4e0cc573a9beafc3efa60afba5
João Vitor <joao.vitor@customax.local>
2023-04-27 11:13:45 -0300

    termino

--- atividade.sql
+++ atividade.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04-Abr-2023 às 16:13
+-- Tempo de geração: 27-Abr-2023 às 16:13
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 7.4.33
 
@@ -30,79 +30,47 @@ SET time_zone = "+00:00";
 CREATE TABLE `datatables` (
   `id` int(11) NOT NULL,
   `nome` varchar(220) NOT NULL,
-  `salario` varchar(220) NOT NULL,
-  `idade` varchar(220) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `Cpf` varchar(14) NOT NULL,
+  `Cep` varchar(9) NOT NULL,
+  `Rua` varchar(300) NOT NULL,
+  `Numero` int(11) NOT NULL,
+  `Bairro` varchar(300) NOT NULL,
+  `Login` varchar(32) NOT NULL,
+  `Senha` varchar(32) NOT NULL,
+  `Telefone` varchar(32) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `Nascimento` varchar(300) NOT NULL,
+  `complemento` varchar(300) NOT NULL,
+  `profile_id` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Extraindo dados da tabela `datatables`
 --
 
-INSERT INTO `datatables` (`id`, `nome`, `salario`, `idade`) VALUES
-(1, 'Marcos Silva', '32880', '45'),
-(2, 'Joao Silva', '27075', '36'),
-(3, 'Maria Silva', '96000', '56'),
-(4, 'Jose Silva', '33306', '21'),
-(5, 'Mauro Silva', '28270', '37'),
-(6, 'Andreia Silva', '68200', '36'),
-(7, 'Isabele Silva', '23750', '21'),
-(8, 'Lucas Silva', '22790', '22'),
-(9, 'Diane Silva', '50550', '39'),
-(10, 'Kelly Silva', '93600', '58'),
-(11, 'Gregorio Silva', '90560', '54'),
-(12, 'Nair Silva', '44200', '29'),
-(13, 'Nelson Silva', '43060', '36'),
-(14, 'Cesar Silva', '53500', '37'),
-(15, 'Anderson Silva', '38575', '20'),
-(16, 'Lurdes Silva', '98500', '59'),
-(17, 'Ana Silva', '82500', '58'),
-(18, 'Gloria Silva', '37750', '31'),
-(19, 'Luiz  Silva', '23200', '20'),
-(20, 'Daine Silva', '21750', '19'),
-(21, 'Veronica Silva', '45000', '30'),
-(22, 'Andre Silva', '75000', '45'),
-(23, 'Wallace Silva', '64500', '32'),
-(24, 'Luiza Silva', '85600', '23'),
-(25, 'Angelica Silva', '59721', '47'),
-(26, 'Romario Silva', '92575', '42'),
-(27, 'Antao Silva', '35765', '28'),
-(28, 'Wagner Silva', '20685', '28'),
-(29, 'Sanzio Silva', '85000', '48'),
-(30, 'Fabiana  Silva', '18800', '20'),
-(31, 'Helio Silva', '95400', '37'),
-(32, 'Orlei Silva', '71450', '53'),
-(33, 'Reanto Silva', '34500', '27'),
-(34, 'Sabino Silva', '22550', '22'),
-(35, 'Turlo Silva', '72405', '46'),
-(36, 'Viviane Silva', '95675', '57'),
-(61, 'joao', '3', '22'),
-(62, 'joao', '6', '6'),
-(63, 'x', '3', '1'),
-(64, 'a', '3', '2'),
-(65, 'pedro', '2000', '30');
+INSERT INTO `datatables` (`id`, `nome`, `Cpf`, `Cep`, `Rua`, `Numero`, `Bairro`, `Login`, `Senha`, `Telefone`, `email`, `Nascimento`, `complemento`, `profile_id`) VALUES
+(43, 'Lurdes', '231.232.132-13', '89252-000', 'Rua Expedicionário Gumercindo da Silva', 2197, 'Tifa Martins', 'Lurdinha', '202cb962ac59075b964b07152d234b70', '(24) 14124-2142', 'Lurdes@gmail.com', '42/14/2141', 'casa', NULL),
+(46, 'Lurdes fofa', '123.123.232-32', '89254-000', 'Rua Erwino Menegotti', 2197, 'Chico de Paulo', '3213', '5327b0d1bfa868acb9baac5a9d901815', '(47) 99981-2223', 'sdasdasdas@gmai', '19/04/2000', 'até 478/479', 'Promotor'),
+(62, 'Lurdes Ribeiro', '099.999.999-99', '89254-000', 'Rua Erwino Menegotti', 2197, 'Chico de Paulo', '00000000000000', '15f47c8a3e5e9685307dd65a653b8dc0', '(22) 22222-2222', '99999999999@gmail', '12/32/1921', 'até 478/479', 'Gere');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuarios`
+-- Estrutura da tabela `perfil`
 --
 
-CREATE TABLE `usuarios` (
-  `id_usario` int(11) NOT NULL,
-  `login` varchar(30) NOT NULL,
-  `senha` varchar(32) NOT NULL,
-  `email` varchar(100) NOT NULL
+CREATE TABLE `perfil` (
+  `id_perfil` int(11) NOT NULL,
+  `perfil` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `usuarios`
+-- Extraindo dados da tabela `perfil`
 --
 
-INSERT INTO `usuarios` (`id_usario`, `login`, `senha`, `email`) VALUES
-(33, 'pedro jogos', 'a880349a0d3f65e2a9bbfa8f81521d03', 'pedrojogos@gmail.com'),
-(34, 'pedro jogos', 'a880349a0d3f65e2a9bbfa8f81521d03', 'ssssssssssssssssss2@gksidjakd.com'),
-(35, 'pedro jogos', 'a880349a0d3f65e2a9bbfa8f81521d03', 'email@gmail.com'),
-(36, 'pedro jogos', 'a880349a0d3f65e2a9bbfa8f81521d03', 'curso.web.2@customax.com.br');
+INSERT INTO `perfil` (`id_perfil`, `perfil`) VALUES
+(129, 'perfil'),
+(128, 'Promotor');
 
 --
 -- Índices para tabelas despejadas
@@ -112,13 +80,17 @@ INSERT INTO `usuarios` (`id_usario`, `login`, `senha`, `email`) VALUES
 -- Índices para tabela `datatables`
 --
 ALTER TABLE `datatables`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Cpf` (`Cpf`),
+  ADD UNIQUE KEY `Login` (`Login`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
--- Índices para tabela `usuarios`
+-- Índices para tabela `perfil`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usario`);
+ALTER TABLE `perfil`
+  ADD PRIMARY KEY (`id_perfil`),
+  ADD UNIQUE KEY `Perfil` (`perfil`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -128,13 +100,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `datatables`
 --
 ALTER TABLE `datatables`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
--- AUTO_INCREMENT de tabela `usuarios`
+-- AUTO_INCREMENT de tabela `perfil`
 --
-ALTER TABLE `usuarios`
-  MODIFY `id_usario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+ALTER TABLE `perfil`
+  MODIFY `id_perfil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
